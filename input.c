3b10dbf359c07aab025f1c558d18dc408943a39d
Sadia Tarin <129206871+Tarin2003@users.noreply.github.com>
2024-09-07 22:45:17 +0600

    Add files via upload

---
+++ input.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i=1;
+
+
+    printf("Hello World\n"); /* print Hello World*/
+    printf("%d\n",i); // print 1
+}
+
---
+++ output.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int i=1;
+    printf("Hello World\n"); // print Hello World
+    printf("%d\n",i); // print i=1
+}
+
